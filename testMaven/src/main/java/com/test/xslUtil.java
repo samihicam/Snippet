7eab04aed47305301cc6a15ba66ed5fda9014ddd
sami <sh.elouadghiri@outlook.fr>
2019-12-14 18:36:00 +0100

    added a class to create an xml file

--- testMaven/src/main/java/com/test/xslUtil.java
+++ testMaven/src/main/java/com/test/xslUtil.java
@@ -12,6 +12,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -25,7 +26,35 @@ public class xslUtil {
 			Document doc =docBuilder.newDocument();
 			Element racine = doc.createElement("repertoire");
 			doc.appendChild(racine);
-			
+			// l'élément contact
+	        Element contact = doc.createElement("contact");
+	        racine.appendChild(contact);
+	 
+	        // attributs de l'élément contact
+	        Attr attr = doc.createAttribute("id");
+	        attr.setValue("1");
+	        contact.setAttributeNode(attr);
+	 
+	        // le nom
+	        Element nom = doc.createElement("nom");
+	        nom.appendChild(doc.createTextNode("codeur"));
+	        contact.appendChild(nom);
+	 
+	        // le prénom
+	        Element prenom = doc.createElement("prenom");
+	        prenom.appendChild(doc.createTextNode("java"));
+	        contact.appendChild(prenom);
+	 
+	        // le mobile
+	        Element mobile = doc.createElement("mobile");
+	        mobile.appendChild(doc.createTextNode("098745126"));
+	        contact.appendChild(mobile);
+	  
+	        // l'email
+	        Element email = doc.createElement("email");
+	        email.appendChild(doc.createTextNode("codeurjava8@gmail.com"));
+	        contact.appendChild(email);
+	        
 			//Write the content into xml file 
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
