b841868b494723af867273d1f7d901cfab46e9fd
sami <sh.elouadghiri@outlook.fr>
2019-12-15 20:52:48 +0100

    ajout de nouveau code dans tutogit

--- testbranchegit/src/main/java/com/sami/java/testBranchModif.java
+++ testbranchegit/src/main/java/com/sami/java/testBranchModif.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("test 2 java class ajout d'un deuxieme changement ");
 	System.out.println("Travail ajouté dans cette branche temporaire de travail");
 	System.out.println("ajout d'un deuxieme changement");
+	System.out.println("Ajout de code tutogit");
 }
 }
 
