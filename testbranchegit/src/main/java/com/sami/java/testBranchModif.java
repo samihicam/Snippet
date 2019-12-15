bc00fdb192a3b29194286c1b6c91ec507f12a6d3
sami <sh.elouadghiri@outlook.fr>
2019-12-15 20:32:19 +0100

    ajout d'un deuxieme changement pour creer un conflit lors du merge

--- testbranchegit/src/main/java/com/sami/java/testBranchModif.java
+++ testbranchegit/src/main/java/com/sami/java/testBranchModif.java
@@ -2,7 +2,8 @@ package com.sami.java;
 
 public class testBranchModif {
 public static void main(String[] args) {
-	System.out.println("test java class");
+	System.out.println("test 2 java class ajout d'un deuxieme changement ");
 	System.out.println("Travail ajouté dans cette branche temporaire de travail");
+	System.out.println("ajout d'un deuxieme changement");
 }
 }
