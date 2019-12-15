b5eae3b177a9fb8ec79d47a7bf36d5e0b9ff0ff4
sami <sh.elouadghiri@outlook.fr>
2019-12-15 20:34:12 +0100

    ajout de nouveau code tutogit

---
+++ testbranchegit/src/main/java/com/sami/java/NouvelleClassDeTest.java
@@ -0,0 +1,7 @@
+package com.sami.java;
+
+public class NouvelleClassDeTest {
+public static void main(String[] args) {
+	System.out.println("ajout d'un nouveau text dans tutogit");
+}
+}
--- testbranchegit/src/main/java/com/sami/java/testBranchModif.java
+++ testbranchegit/src/main/java/com/sami/java/testBranchModif.java
@@ -2,6 +2,6 @@ package com.sami.java;
 
 public class testBranchModif {
 public static void main(String[] args) {
-	System.out.println("test java class");
+	System.out.println("test java class nouveau text branche tutogit");
 }
 }
