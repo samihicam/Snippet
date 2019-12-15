c73562ca482a285d81839338e0ac6a10e3908c5f
sami <sh.elouadghiri@outlook.fr>
2019-12-15 20:34:37 +0100

    nouveau commit tutogit

--- testbranchegit/src/main/java/com/sami/java/NouvelleClassDeTest.java
+++ testbranchegit/src/main/java/com/sami/java/NouvelleClassDeTest.java
@@ -3,5 +3,6 @@ package com.sami.java;
 public class NouvelleClassDeTest {
 public static void main(String[] args) {
 	System.out.println("ajout d'un nouveau text dans tutogit");
+	System.out.println("ajout d'un nouveau text dans tutogit2");
 }
 }
