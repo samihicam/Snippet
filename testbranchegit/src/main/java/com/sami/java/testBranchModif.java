606811afd290157e8d1ea8a65509b06fbf4d4176
sami <sh.elouadghiri@outlook.fr>
2019-12-15 20:29:47 +0100

    ajout de traitement additionnel

--- testbranchegit/src/main/java/com/sami/java/testBranchModif.java
+++ testbranchegit/src/main/java/com/sami/java/testBranchModif.java
@@ -3,5 +3,6 @@ package com.sami.java;
 public class testBranchModif {
 public static void main(String[] args) {
 	System.out.println("test java class");
+	System.out.println("Travail ajouté dans cette branche temporaire de travail");
 }
 }
